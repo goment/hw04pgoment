acfa7d3eaaabcd50619576d606b09cb1bc141545
Miantong Guo <guomiantong@gmail.com>
2022-10-05 17:25:09 -0400

    changed function 1

--- R/data_tibble.R
+++ R/data_tibble.R
@@ -1,8 +1,12 @@
 data_tibble <- function(data){
-  var_name <- ls(data)
-  class1 <- sapply(data,class)
-  var_unique <- lengths(sapply(data, unique))
-  var_na <- colSums(is.na(data))
-  result <- tibble(var_name, class1, var_unique, var_na)
+  if (ncol(data) > 5 & nrow(data) > 9 & length(unique(sapply(data,class)))){
+    var_name <- ls(data)
+    class1 <- sapply(data,class)
+    var_unique <- lengths(sapply(data, unique))
+    var_na <- colSums(is.na(data))
+    result <- tibble(var_name, class1, var_unique, var_na)
+  }else{
+    result <- "Stop, your data does not match requirement."
+  }
   return(result)
 }
