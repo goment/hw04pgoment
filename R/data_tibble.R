9243fd404d83971483400facb2ba05e9b30f3754
Miantong Guo <guomiantong@gmail.com>
2022-10-05 20:33:11 -0400

    updated help

--- R/data_tibble.R
+++ R/data_tibble.R
@@ -1,3 +1,14 @@
+#' The summary of variables
+#'
+#' @param data a data frame
+#' @import tidyverse
+#' @return a table with data variables and name/class/number of unique values/number of NA of variables
+#' @export
+#'
+#' @examples
+#' data_tibble(starwars)
+#' data_tibble(billboard)
+
 data_tibble <- function(data){
   if (ncol(data) > 5 & nrow(data) > 9 & length(unique(sapply(data,class)))){
     var_name <- ls(data)
@@ -6,7 +17,12 @@ data_tibble <- function(data){
     var_na <- colSums(is.na(data))
     result <- tibble(var_name, class1, var_unique, var_na)
   }else{
-    result <- "Stop, your data does not match requirement."
+    result <- stop("Your data does not match requirement")
   }
   return(result)
 }
+
+
+#test it
+data_tibble(dplyr::starwars)
+#data_tibble(dplyr::band_members)
